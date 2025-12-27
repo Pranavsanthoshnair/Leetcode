c4988cfd7362352ad8218c82a02173e66f3d340e
Pranav S Nair <pranavsnair2028@cs.sjcetpalai.ac.in>
2025-12-27 11:23:25 +0530

    Pow_X_N

---
+++ Pow_x_n.cpp
@@ -0,0 +1,59 @@
+
+
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if (n == 0) {
+            return 1.0;
+        }
+        if (x == 0) {
+            return 0.0;
+        }
+        if (x == 1) {
+            return 1.0;
+        }
+        if (x == -1 && n % 2 == 0) {
+            return 1.0;
+        }
+        if (x == -1 && n % 2 != 0) {
+            return -1.0;
+        }
+
+        long bf = n;
+        if (n < 0) {
+            x = 1 / x;
+            bf = -bf;
+        }
+
+        double ans = 1.0;
+        while (bf > 0) {
+            if (bf % 2 == 1) {
+                ans *= x;
+            }
+            x *= x;
+            bf /= 2;
+        }
+        return ans;
+    }
+};
+
+int main() {
+    Solution obj;
+
+    double x;
+    int n;
+
+    cout << "Enter base x: ";
+    cin >> x;
+
+    cout << "Enter power n: ";
+    cin >> n;
+
+    double result = obj.myPow(x, n);
+    cout << "Result: " << result << endl;
+
+    return 0;
+}
