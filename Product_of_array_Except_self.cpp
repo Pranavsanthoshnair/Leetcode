42d6e669edbea054b7dc63083edb3b29d488476b
Pranav S Nair <pranavsnair2028@cs.sjcetpalai.ac.in>
2026-01-04 09:36:55 +0530

    Product of Array Except Self

---
+++ Product_of_array_Except_self.cpp
@@ -0,0 +1,127 @@
+/*
+Leetcode Solution
+Problem No : 238
+Problem Name : Product of Array Except Self
+Difficulty : Medium
+*/
+
+// Solution 1 (Gives TLE)
+/*
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n, 1);
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(i != j){
+                    ans[i] *= nums[j];
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+int main() {
+    Solution obj;
+
+    vector<int> nums = {1, 2, 3, 4};
+
+    vector<int> result = obj.productExceptSelf(nums);
+
+    for(int x : result){
+        cout << x << " ";
+    }
+
+    return 0;
+}
+*/
+
+// Solution 2 (Optimised only time complexity)
+
+/*
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n, 1);
+        vector<int> prefix(n, 1);
+        vector<int> suffix(n, 1);
+
+        for(int i = 1; i < n; i++){
+            prefix[i] = prefix[i - 1] * nums[i - 1];
+        }
+
+        for(int i = n - 2; i >= 0; i--){
+            suffix[i] = suffix[i + 1] * nums[i + 1];
+        }
+
+        for(int i = 0; i < n; i++){
+            ans[i] = prefix[i] * suffix[i];
+        }
+
+        return ans;
+    }
+};
+
+int main() {
+    Solution obj;
+
+    vector<int> nums = {1, 2, 3, 4};
+
+    vector<int> result = obj.productExceptSelf(nums);
+
+    for(int x : result){
+        cout << x << " ";
+    }
+
+    return 0;
+}
+*/
+
+// Solution 3 (Optimised both time ans space complexity)
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n, 1);
+
+        for(int i = 1; i < n; i++){
+            ans[i] = ans[i - 1] * nums[i - 1];
+        }
+
+        int suffix = 1;
+        for(int i = n - 2; i >= 0; i--){
+            suffix *= nums[i + 1];
+            ans[i] *= suffix;
+        }
+
+        return ans;
+    }
+};
+
+int main() {
+    Solution obj;
+    vector<int> nums = {1, 2, 3, 4};
+    vector<int> result = obj.productExceptSelf(nums);
+    for(int x : result){
+        cout << x << " ";
+    }
+    return 0;
+}
+
