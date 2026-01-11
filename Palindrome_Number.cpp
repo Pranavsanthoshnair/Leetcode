b5bbaf3e28579a52987fc0f70d71d4a054ea997d
Pranav S Nair <pranavsnair2028@cs.sjcetpalai.ac.in>
2026-01-11 18:18:29 +0530

    Create Palindrome_Number.cpp

---
+++ Palindrome_Number.cpp
@@ -0,0 +1,24 @@
+
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) return false;
+
+        int temp = x;
+        long rev = 0;
+
+        while (x > 0) {
+            rev = rev * 10 + (x % 10);
+            x /= 10;
+        }
+
+        return temp == rev;
+    }
+};
